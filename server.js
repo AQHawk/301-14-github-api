627eec7606cbd409f11f39ab48136d8fb29fa797
AQHawk <ironhawkcreations@gmail.com>
2017-11-10 11:39:25 -0800

    API Token Demo

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const cors = require('cors');
+const superagent = require('superagent');
 
 app.use(express.static('./public'));
 app.use(cors());
+
+// Proxy route for handling github API requests
+app.get('/github/*', (req, res) => {
+  console.log('Routing a github API req for', req.params[0]);
+  const url = `https://api.github.com/${req.params[0]}`;
+  superagent(url)
+    .set('Authorization', `token ${process.env.GITHUB_TOKEN}`)
+  // .then (success-callback, fail-callback)
+    .then(
+      repos => res.send(repos.text),
+      err => res.send(err)
+    )
+})
 
 app.get('/', (req, res) => {
   res.sendFile('index.html')
